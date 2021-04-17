34a8935edb45239f1f89818e8343c154f25edc4c
ahmedbellaaj10 <ahmed.belaaj@insat.u-carthage.tn>
2021-04-17 17:00:55 +0100

    added signup and login for doctor

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,110 +1,59 @@
-const { getAllPatients, getPatientById, addPatient, updateOrdonnance, askforJADAS, validateJADAS, fillJADAS, askforBILAN, fillBILAN } = require("../queries/patientQueries") 
-const express = require('express')
+const {
+  getAllPatients,
+  getPatientById,
+  addPatient,
+  updateOrdonnance,
+  askforJADAS,
+  validateJADAS,
+  fillJADAS,
+  askforBILAN,
+  fillBILAN,
+} = require("../queries/patientQueries");
+const express = require("express");
 const patientRoutes = express.Router();
 
-
-/* ************************************************************************************************* */ 
-const multer =  require('multer');
+/* ************************************************************************************************* */
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + file.originalname);
-    }
- });
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-const fileFilter = (req, file, cb)=>{
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-        cb(null, true);
-    }else {
-        cb(null, false);
-    }
-}
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
-const upload = multer ({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
-    fileFileter: fileFilter
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFileter: fileFilter,
 });
 
 /* ************************************************************************************************ */
 
-
-patientRoutes.route("/").get((req, res)=>{
-    getAllPatients(req,res)
-    
+patientRoutes.route("/:id").get((req, res) => {
+  getPatientById(req.params.id, res);
 });
 
-patientRoutes.route("/:id").get((req,res)=>{
-    getPatientById(req.params.id,res);
+patientRoutes.route("/fillJADAS/:idp/:idj").post((req, res) => {
+  fillJADAS(req.params.idp, req.params.idj, req.query.score, res);
 });
 
-patientRoutes.route("/add").post((req,res)=>{
-    addPatient(
-        req.query.nom,
-        req.query.prenom,
-        req.query.date_naissance,
-        req.query.telephone,
-        req.query.num_dossier,
-        req.query.diagnostic,
-        req.query.ordonnance,
-        res
-    );
-});
-
-patientRoutes.route("/updateOrdonnance/:id").post((req,res)=>{
-    updateOrdonnance(
-        req.params.id,
-        req.query.ordonnance,
-        res);
-});
-
-patientRoutes.route("/newJADAS/:id").post((req,res)=>{
-    askforJADAS(
-        req.params.id,
-        req.query.date,
-        res
-        );
-});
-
-patientRoutes.route("/fillJADAS/:idp/:idj").post((req,res)=>{
-    fillJADAS(
-        req.params.idp,
-        req.params.idj,
-        req.query.score,
-        res
-        );
-});
-
-patientRoutes.route("/validJADAS/:idp/:idj").post((req,res)=>{
-    validateJADAS(
-        req.params.idp,
-        req.params.idj,
-        res
-        );
-});
-
-patientRoutes.route("/newBilan/:id").post((req,res)=>{
-    askforBILAN(
-        req.params.id,
-        req.query.type,
-        req.query.date,
-        res
-        );
-});
-
-patientRoutes.route("/fillBILAN/:idp/:idb").post(upload.single('bilan'),(req,res)=>{
-    fillBILAN(
-        req.params.idp,
-        req.params.idb,
-        req.file.path,
-        res
-        );
-});
-
+patientRoutes
+  .route("/fillBILAN/:idp/:idb")
+  .post(upload.single("bilan"), (req, res) => {
+    fillBILAN(req.params.idp, req.params.idb, req.file.path, res);
+  });
 
 module.exports = patientRoutes;
--- server.js
+++ server.js
@@ -1,23 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const patientRoutes = require ("./routes/patientRoutes");
-const cors = require('cors');
+const patientRoutes = require("./routes/patientRoutes");
+const doctorRoutes = require("./routes/doctorRoutes");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const PORT = 4000;
 app.use(cors());
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
+app.use(express.json());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+app.listen(PORT, function () {
+  console.log("Server is running on Port: " + PORT);
 });
 
-const uri = "mongodb+srv://express:bookrecsys123@cluster0.9xnsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-let mongoose = require("mongoose")
+const uri =
+  "mongodb+srv://express:bookrecsys123@cluster0.9xnsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://chaima:bookrecsys123@cluster0-z4urb.gcp.mongodb.net/test?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-}).then(() => {
-    console.log('Database sucessfully connected')
-},
-    error => {
-        console.log('Database could not be connected: ' + error)
+mongoose
+  .connect(
+    "mongodb+srv://chaima:bookrecsys123@cluster0-z4urb.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
-)
-app.use('/patients', patientRoutes);
+  )
+  .then(
+    () => {
+      console.log("Database sucessfully connected");
+    },
+    (error) => {
+      console.log("Database could not be connected: " + error);
+    }
+  );
+app.use("/patients", patientRoutes);
+app.use("/doctors", doctorRoutes);
